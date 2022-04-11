8903afa8c54236598be373489d1c16ee32d47dd5
neilfang <neilfang@tencent.com>
2022-04-11 18:06:38 +0800

    simplify cart coordinates generation code

--- project_viz/umath/spherical_plot.py
+++ project_viz/umath/spherical_plot.py
@@ -12,21 +12,22 @@ def heatmap(distribution_func, axis3d, **params):
     cos_phi = np.cos(phi)
 
     # data values
+    x_grid = np.zeros((count, count))
+    y_grid = np.zeros((count, count))
+    z_grid = np.zeros((count, count))
+
     data = np.zeros((count, count))
     for j, theta_v in enumerate(zip(sin_theta, cos_theta)):
         for i, phi_v in enumerate(zip(sin_phi, cos_phi)):
             x = phi_v[0] * theta_v[1]
             y = phi_v[0] * theta_v[0]
             z = phi_v[1]
+
+            x_grid[i, j] = x
+            y_grid[i, j] = y
+            z_grid[i, j] = z
+
             data[i, j] = distribution_func([x, y, z], **params)
-
-    # mesh grid
-    sin_theta_grid, sin_phi_grid = np.meshgrid(sin_theta, sin_phi)
-    cos_theta_grid, cos_phi_grid = np.meshgrid(cos_theta, cos_phi)
-
-    x_grid = sin_phi_grid * cos_theta_grid
-    y_grid = sin_phi_grid * sin_theta_grid
-    z_grid = cos_phi_grid
 
     # face colors
     coolwarm = plt.get_cmap('coolwarm')
